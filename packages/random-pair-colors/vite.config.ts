870b974a3f10937eaf0bed93c6b7dc918cb9fbc7
ergofriend <ergofriend@kasu.dev>
2024-01-26 23:31:18 +0900

    fix build @lys-42/random-pair-colors

--- packages/random-pair-colors/vite.config.ts
+++ packages/random-pair-colors/vite.config.ts
@@ -1,5 +1,6 @@
 import { terser } from 'rollup-plugin-terser'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,10 +10,12 @@ export default defineConfig({
 		lib: {
 			entry: 'src/index.ts',
 			name: '@lys-42/random-pair-colors',
+			fileName: 'main',
 		},
 	},
 	plugins: [
 		// @ts-expect-error
 		terser(),
+		dts({ rollupTypes: true }),
 	],
 })
