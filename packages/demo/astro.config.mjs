83dcf491281d03608d086e956d21a85921b3268f
ergofriend <ergofriend@kasu.dev>
2024-02-18 21:16:35 +0900

    add base path for GitHub Pages

--- packages/demo/astro.config.mjs
+++ packages/demo/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config'
 import tokyoNight from 'shiki/dist/themes/tokyo-night.mjs'
 
 export default defineConfig({
+	site: 'https://ergofriend.github.io',
+	base: '/lys-colors',
 	markdown: {
 		shikiConfig: {
 			theme: tokyoNight,
