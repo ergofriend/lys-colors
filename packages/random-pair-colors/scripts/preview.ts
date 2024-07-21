176a2138586fc28f2677c59453bfaa1aa093d442
ergofriend <ergofriend@kasu.dev>
2024-07-21 23:50:52 +0900

    fix infinite loop

--- packages/random-pair-colors/scripts/preview.ts
+++ packages/random-pair-colors/scripts/preview.ts
@@ -1,13 +1,29 @@
 import { generateRandomPairColor } from '../src/index'
 
+const TEST_COLOR = '#808080'
+
+const log = (result: { textColor: string; backgroundColor: string }) => {
+	console.log(
+		`%c text: ${result.textColor} background: ${result.backgroundColor}`,
+		`color: ${result.textColor}; background-color: ${result.backgroundColor}`,
+	)
+}
+
 for (let i = 0; i < 10; i++) {
 	const result = generateRandomPairColor({
-		backgroundColor: '#3f2697',
+		textColor: TEST_COLOR,
 		threshold: 70,
 	})
 
-	console.log(
-		`%cHello Deno ${result.textColor}(${result.backgroundColor}) !!!`,
-		`color: ${result.textColor}; background-color: ${result.backgroundColor}`,
-	)
+	log(result)
+}
+
+for (let i = 0; i < 10; i++) {
+	const result = generateRandomPairColor({
+		backgroundColor: TEST_COLOR,
+		threshold: 70,
+	})
+
+	// TODO: fix the text color output
+	log(result)
 }
